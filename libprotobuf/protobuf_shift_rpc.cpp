954053736006073fc61fa5a8ad160670a972727e
dongqi.zheng <dongqi.zheng@mxplayer.in>
2022-05-22 23:37:35 +0800

    update for pb

---
+++ libprotobuf/protobuf_shift_rpc.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ @Author  : zhengdongqi
+ @Email   : 
+ @Usage   :
+ @FileName: protobuf_shift_rpc.cpp
+ @DateTime: 2022/5/22 23:36
+ @SoftWare: CLion
+************************************************************************/
+
+//
+// Created by 郑东琦 on 2022/5/22.
+//
+
+#include "protobuf_shift_rpc.h"
---
+++ libprotobuf/protobuf_shift_rpc.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+ @Author  : zhengdongqi
+ @Email   : 
+ @Usage   :
+ @FileName: protobuf_shift_rpc.cpp
+ @DateTime: 2022/5/22 23:36
+ @SoftWare: CLion
+************************************************************************/
+
+//
+// Created by 郑东琦 on 2022/5/22.
+//
+
+#ifndef PROTOBUF_SHIFT_RPC_H
+#define PROTOBUF_SHIFT_RPC_H
+
+
+class ProtobufShiftRpc {
+
+};
+
+
+#endif //PROTOBUF_SHIFT_RPC_H
